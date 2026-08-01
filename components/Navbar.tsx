15efa28a99ade9ccca094028574b00c99e82979b
PavanBhat007 <pvn.24.offi@gmail.com>
2026-08-01 08:00:11 +0530

    fix: Navbar moved to layout to easily accomodate for new pages

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+export default function Navbar() {
+  return (
+    <header className="bg-linear-to-br from-amber-50 via-white to-amber-100 text-gray-950 w-full border border-amber-300 shadow-sm shadow-amber-300 px-6 py-12">
+      <div className="flex justify-between items-start">
+        <div>
+          <h1 className="font-[oswald] font-bold text-5xl">
+            MICRO SKILL AGENT
+          </h1>
+          <p className="mt-2">
+            Learn one &apos;
+            <span className="font-semibold underline italic text-orange-500">
+              Micro Skill
+            </span>
+            &apos; every day
+          </p>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Show when="signed-out">
+            <SignInButton mode="modal">
+              <button className="px-4 py-1.5 border border-orange-400 rounded text-orange-600 font-medium hover:bg-orange-50">
+                Login
+              </button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <button className="px-4 py-1.5 bg-orange-500 text-white rounded font-medium hover:bg-orange-600">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </Show>
+
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </div>
+      </div>
+    </header>
+  )
+}
